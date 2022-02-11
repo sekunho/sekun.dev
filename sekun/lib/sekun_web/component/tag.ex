0eb6bf02baa79cfac4432271d807925ff4eaf7b7
SEKUN <sekun@hey.com>
2022-02-11 09:51:00 +0800

    Change dark theme tag color

--- sekun/lib/sekun_web/component/tag.ex
+++ sekun/lib/sekun_web/component/tag.ex
@@ -4,7 +4,7 @@ defmodule SekunWeb.Component.Tag do
   def tag(assigns) do
     ~H"""
     <span
-      class="inline-flex items-center px-2.5 py-0.5 rounded-md text-xs sm:text-sm font-medium bg-su-accent-1/[0.2] dark:bg-su-dark-accent-1/[0.2] text-su-accent-1/[0.7] dark:text-su-dark-fg/[0.7]">
+      class="inline-flex items-center px-2.5 py-0.5 rounded-md text-xs sm:text-sm font-medium bg-su-accent-1/[0.2] dark:bg-su-dark-bg-alt/[0.5] text-su-accent-1/[0.7] dark:text-su-dark-fg/[0.7]">
       <%= render_slot(@inner_block) %>
     </span>
     """
