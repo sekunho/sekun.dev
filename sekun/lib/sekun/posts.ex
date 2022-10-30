a0e6ee1a67e02e0bf1741c70d1bb67270994f0a0
SEKUN <sekun@hey.com>
2022-10-30 22:47:30 +0800

    fix: Update old blog references

--- sekun/lib/sekun/posts.ex
+++ sekun/lib/sekun/posts.ex
@@ -2,7 +2,7 @@ defmodule Sekun.Posts do
   def list() do
     {:ok, res} =
       :get
-      |> Finch.build("https://blog.sekun.dev/index.xml")
+      |> Finch.build("https://blog.sekun.net/index.xml")
       |> Finch.request(MyFinch)
 
     {:ok, posts} = FastRSS.parse(res.body)
